0fc866ec1c496a0fa42bc85a141d185ddeb575fa
Pete Hurst <pete.hurst@gmail.com>
2014-11-27 23:56:35 +0000

    Add a simple root page test with supertest

---
+++ test/1_server.js
@@ -0,0 +1,13 @@
+var skytestApp = require('../app/application.js')(),
+  request = require('supertest');
+
+describe('Application', function(){
+
+  it('Serves an index page', function(done){
+    request(skytestApp)
+      .get('/')
+      .expect('Content-Type', 'text/html')
+      .expect(200, done);
+  });
+
+});
