bd33739a300f5779b788917323d0e30e664e81fa
Pete Hurst <pete.hurst@gmail.com>
2014-11-27 23:52:26 +0000

    Setup basic Express app

---
+++ app/application.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+module.exports = function() {
+
+  skytest = express();
+  return skytest();
+
+};
---
+++ server.js
@@ -0,0 +1,4 @@
+var skytestApp = require('./app/application.js')();
+
+skytestApp.listen(3000);
+console.log("Server running on http://localhost:3000");
