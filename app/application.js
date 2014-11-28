b76a9ef53bd4a87840580dcd00989ece4aea829d
Pete Hurst <pete.hurst@gmail.com>
2014-11-28 00:13:42 +0000

    Serve up an index page from a Jade template, correct test to use a regexp

--- app/application.js
+++ app/application.js
@@ -3,6 +3,15 @@ var express = require('express');
 module.exports = function() {
 
   skytest = express();
-  return skytest();
+
+  // Configure Express rendering
+  skytest.set('views', __dirname + '/views');
+  skytest.set('view engine', 'jade');
+
+  skytest.get('/', function(req,res,next) {
+    res.render('index.jade');
+  });
+
+  return skytest;
 
 };
--- test/1_server.js
+++ test/1_server.js
@@ -6,7 +6,7 @@ describe('Application', function(){
   it('Serves an index page', function(done){
     request(skytestApp)
       .get('/')
-      .expect('Content-Type', 'text/html')
+      .expect('Content-Type', /text\/html/)
       .expect(200, done);
   });
 
