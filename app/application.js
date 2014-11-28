a159329fbe81b97de05c16b338be7599471063e9
Pete Hurst <pete.hurst@gmail.com>
2014-11-28 01:55:17 +0000

    Add static route and add scripts to index

--- app/application.js
+++ app/application.js
@@ -8,9 +8,13 @@ module.exports = function() {
   skytest.set('views', __dirname + '/views');
   skytest.set('view engine', 'jade');
 
+  // Serve home page
   skytest.get('/', function(req,res,next) {
     res.render('index.jade');
   });
+
+  // Serve static files in public
+  skytest.use(express.static(__dirname + '/public'));
 
   return skytest;
 
